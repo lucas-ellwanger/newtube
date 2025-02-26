885dbeff412cc59ab5a7dd308a8033d0007d53ca
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2025-02-26 18:51:11 -0300

    Setup additional tRPC configuration, add auth to tRPC context, add protected procedure, add Upstash rate limit

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,11 +1,12 @@
 "use client";
 // ^-- to make sure we can mount the Provider from a server component
 
+import { useState } from "react";
+import superjson from "superjson";
 import type { QueryClient } from "@tanstack/react-query";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
-import { useState } from "react";
 import { makeQueryClient } from "./query-client";
 import type { AppRouter } from "./routers/_app";
 
@@ -45,7 +46,7 @@ export function TRPCProvider(
     trpc.createClient({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson,
           url: getUrl(),
         }),
       ],
