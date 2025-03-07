7b06614d7d981688fd4902f645c79339306cb346
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2025-03-07 01:19:27 -0300

    Renamed env key, no need to have NEXT_PUBLIC as prefix, wont be used on the client side

--- src/lib/deepseek.ts
+++ src/lib/deepseek.ts
@@ -1,6 +1,6 @@
 import { OpenAI } from "openai";
 
 export const deepseek = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENROUTER_API_KEY,
+  apiKey: process.env.DEEPSEEK_API_KEY,
   baseURL: "https://openrouter.ai/api/v1",
 });
