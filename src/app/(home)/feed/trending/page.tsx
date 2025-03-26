56757c7ab06f785b27b64c92d3cc4e27e6933cb0
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2025-03-26 16:49:49 -0300

    Add trending videos page with infinite scroll and view section

---
+++ src/app/(home)/feed/trending/page.tsx
@@ -0,0 +1,17 @@
+import { DEFAULT_LIMIT } from "@/constants";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { TrendingView } from "@/modules/home/ui/views/trending-view";
+
+export const dynamic = "force-dynamic";
+
+export default async function Page() {
+  void trpc.videos.getManyTrending.prefetchInfinite({
+    limit: DEFAULT_LIMIT,
+  });
+
+  return (
+    <HydrateClient>
+      <TrendingView />
+    </HydrateClient>
+  );
+}
