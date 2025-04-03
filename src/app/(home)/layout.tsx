7a67bad260df0d8036a3fe57d03de7bb75f36cec
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2025-04-03 17:53:05 -0300

    Remove unnecessary dynamic export from home layout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,8 +1,5 @@
 import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
 
-// TODO: Confirm if this is needed
-export const dynamic = "force-dynamic";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <HomeLayout>{children}</HomeLayout>;
 }
