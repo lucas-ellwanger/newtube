2967083cda663be581632f34e0d75b0c25f181a1
Lucas Ellwanger <lucasellwanger.dev@gmail.com>
2025-08-18 19:48:00 -0300

    chore: update search button border color to match search input border

--- src/modules/home/ui/components/home-navbar/search-input.tsx
+++ src/modules/home/ui/components/home-navbar/search-input.tsx
@@ -63,7 +63,7 @@ export const SearchInput = () => {
       <button
         disabled={!value.trim()}
         type="submit"
-        className="px-5 py-2.5 bg-gray-100 border border-l-0 rounded-r-full hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed"
+        className="px-5 py-2.5 bg-gray-100 border border-neutral-300 border-l-0 rounded-r-full hover:bg-gray-200 disabled:opacity-50 disabled:cursor-not-allowed"
       >
         <SearchIcon className="size-5" />
       </button>
